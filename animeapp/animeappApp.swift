5c5ad97b6aed100a2ad71f10196df011e8c30650
Vignesh Iyer <vgnshiyer@gmail.com>
2025-11-20 19:13:00 -0800

    Initial Commit

---
+++ animeapp/animeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  animeappApp.swift
+//  animeapp
+//
+//  Created by Vignesh Iyer on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct animeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ animeappTests/animeappTests.swift
@@ -0,0 +1,17 @@
+//
+//  animeappTests.swift
+//  animeappTests
+//
+//  Created by Vignesh Iyer on 11/20/25.
+//
+
+import Testing
+@testable import animeapp
+
+struct animeappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
